4848a019870cf5657f6092a3557799d309651fed
Pedro Assumpcao <pedro.assumpcao@gmail.com>
2021-03-15 15:35:25 -0500

    Fix bug by overriding Repo default options.

--- lib/ecto_bug/repo.ex
+++ lib/ecto_bug/repo.ex
@@ -2,4 +2,11 @@ defmodule EctoBug.Repo do
   use Ecto.Repo,
     otp_app: :ecto_bug,
     adapter: Ecto.Adapters.Postgres
+
+  def default_options(operation) do
+    case Mix.env() do
+      :test -> super(operation) ++ [in_parallel: false]
+      _env -> super(operation)
+    end
+  end
 end
